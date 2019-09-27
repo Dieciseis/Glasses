e619fafd8e8ec375c3653eed7cf1a2f38f61f3f0
Dieciseis <2668108617@qq.com>
2019-09-28 00:28:24 +0800

    服务端处理程序。

--- insertData.php
+++ insertData.php
@@ -1,1 +1,38 @@
 <?php
+header("content-Type: text/html; charset=utf-8");//字符编码设置
+
+require_once 'DBC.php';
+$db = new DBC();
+
+$tid=$_GET['tid'];
+// 创建连接
+$conn =new mysqli($db->servername, $db->username, $db->password, $db->dbname);
+// 检测连接
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$tags = explode(",",$tid);
+$size = count($tags);
+$sql = "SELECT cid FROM `tag_tie` WHERE tid=".$tags[0];
+for($k = 1;$k<$size;$k++){
+    $sql = $sql." OR tid=".$tags[$k];
+}
+$sql = $sql." ;";
+
+$result = $conn->query($sql);
+
+
+$arr = array();
+// 输出每行数据
+while($row = $result->fetch_assoc()) {
+    $count=count($row);//不能在循环语句中，由于每次删除row数组长度都减小
+    for($i=0;$i<$count;$i++){
+        unset($row[$i]);//删除冗余数据
+    }
+    array_push($arr,$row);
+
+}
+//print_r($arr);
+echo json_encode($arr,JSON_UNESCAPED_UNICODE);//json编码
+$conn->close();
